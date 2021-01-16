86436c1e6546d5f01b091cf2bfc651cb647d2231
BetulKahya <betulkahya11@gmail.com>
2021-01-16 19:25:47 +0300

    Update naive_bayes.R

--- Sınıflandırma_Algoritmaları/naive_bayes.R
+++ Sınıflandırma_Algoritmaları/naive_bayes.R
@@ -10,26 +10,17 @@ str(data)
 data$death_event <- as.factor(data$death_event)
 str(data)
 
-#Görselleþtirme
-pairs.panels(data[7:12])
-data %>%
-  ggplot(aes(x=ejection_fraction,y=serum_creatinine,fill=death_event))+
-  geom_boxplot()+
-  ggtitle("Box Plot")
-data %>%
-  ggplot(aes(x=serum_creatinine,fill=death_event))+
-  geom_density(alpha=0.8,color='black')+
-  ggtitle("density plot")
-
 #Test Train Veri Bölümü
 set.seed(123)
 ind <- sample(2,nrow(data),replace = T,prob = c(0.8,0.2))
 train <- data[ind==1,]
 test <- data[ind==2,]
 
+
 #Naive Bayes Model
 model <- naive_bayes(death_event~.,data = train)
 model
+
 
 
 #Confusion matrix-test data
